a1bbb639ad43f9e3dc24dc831a9fc5abb37b461c
nguyenviet3057 <nguyenviet3057@gmail.com>
2022-11-15 13:26:13 +0700

    try fixing blank screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
-import Home from './Components/Home/Home'
+import CProductList from './Components/Product/ProductList'
+import CAdd from './Components/Form/Add'
+import CEdit from './Components/Form/Edit'
+import CDelete from './Components/Form/Delete'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 export default class App extends Component {
 
   render() {
-    return <Home />
+    return (
+      <Router>
+        <Routes>
+          <Route path='/' element={<CProductList />} />
+          <Route path='/edit/:id' element={<CEdit />} />
+          <Route path='/add' element={<CAdd />} />
+          <Route path='/delete/:id' element={<CDelete />} />
+        </Routes>
+      </Router>
+    )
   }
 }
